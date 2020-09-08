c4adbc922dd8106c4a26dbabf44d0953cba6bacb
mailys <mailys.perez@epitech.eu>
2020-09-08 21:14:29 +0200

    initial xcode project

---
+++ app-pixel-city/app-pixel-city/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-pixel-city
+//
+//  Created by Maïlys Perez on 08/09/2020.
+//  Copyright © 2020 Maïlys Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
